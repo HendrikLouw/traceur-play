995c732e9129c8d7cf3a81cfd88c00d20eb5ee81
Hendrik Louw <hflouw@gmail.com>
2014-10-14 20:53:24 +0200

    uglify

--- gulp/build.js
+++ gulp/build.js
@@ -2,12 +2,14 @@ var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var traceur = require('gulp-traceur');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 gulp.task('build', function() {
   return gulp.src('src/**/*.js')
     .pipe(sourcemaps.init())
     .pipe(traceur())
-    .pipe(concat('app.js'))
+    .pipe(concat('app.min.js'))
+    .pipe(uglify())
     .pipe(sourcemaps.write())
-    .pipe(gulp.dest('dist'));
+    .pipe(gulp.dest('dist/'));
 });
