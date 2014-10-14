f5758f2b5a54c083865db06f944d7efb16215e9d
Hendrik Louw <hflouw@gmail.com>
2014-10-14 20:49:12 +0200

    concatenation with sourcemaps

--- gulp/build.js
+++ gulp/build.js
@@ -1,8 +1,13 @@
 var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
 var traceur = require('gulp-traceur');
+var concat = require('gulp-concat');
 
-gulp.task('build', function () {
-    return gulp.src('src/**/*.js')
-        .pipe(traceur())
-        .pipe(gulp.dest('dist'));
+gulp.task('build', function() {
+  return gulp.src('src/**/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(traceur())
+    .pipe(concat('app.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist'));
 });
